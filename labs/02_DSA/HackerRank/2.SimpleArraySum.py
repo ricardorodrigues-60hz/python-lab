1d6a1fded7b9e017142bebb7ee813d41b47b8d83
Ricardo <ricardofrodrigues.contato@gmail.com>
2026-08-31 22:45:31 -0300

    DSA

---
+++ labs/02_DSA/HackerRank/2.SimpleArraySum.py
@@ -0,0 +1,32 @@
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'simpleArraySum' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY ar as parameter.
+#
+
+def simpleArraySum(ar):
+    # Write your code here
+    for i in ar:
+        sum = int(0)
+        sum += i
+    return sum
+        
+
+if __name__ == '__main__':
+
+    ar_count = int(input('Digite um numero ai pra eu testa').strip())
+
+    ar = list(map(int, input('fitia mais').rstrip().split()))
+
+    result = simpleArraySum(ar)
+
+    print(result)
+
+
---
+++ labs/02_DSA/HackerRank/sandbox.py
@@ -0,0 +1,6 @@
+
+n = 10
+i = 0
+for i in range(i, n+i):
+    print(i)
+    print(n)
