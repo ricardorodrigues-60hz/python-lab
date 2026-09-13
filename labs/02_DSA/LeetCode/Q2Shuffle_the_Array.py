ad3a52ef11dc2aa7212d45824a66543b50364a93
Ricardo <ricardofrodrigues.contato@gmail.com>
2026-09-13 17:37:59 -0300

    ShuffleArray incompleta

---
+++ labs/02_DSA/LeetCode/Q2Shuffle_the_Array.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+def shuffle(nums: List[int], n: int) -> List[int]:
+    nums2 = nums[:n]
+    nums = nums[n:]
+    print(nums2)
+    print(nums)
+
+    return List(lambda l: ...)
+
+shuffle([1,2,3,4,5,6], 3)
+
