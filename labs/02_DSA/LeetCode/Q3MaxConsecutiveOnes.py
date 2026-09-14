1be8f44d402f5a3ae2c276065ce52c50a7b5b0aa
ricardo <101021873+ricardorodrigues-60hz@users.noreply.github.com>
2026-09-14 17:16:00 -0300

    Q3Max ConsecutivesOnes

---
+++ labs/02_DSA/LeetCode/Q3MaxConsecutiveOnes.py
@@ -0,0 +1,15 @@
+from typing import List
+
+def findMaxConsecutiveOnes(nums: List[int]) -> int:
+    max_count = 0
+    count = 0
+    for num in nums:
+        if num == 1:
+            count += 1
+            max_count = max(max_count, count)
+        else:
+            count = 0
+            
+    return max_count
+
+print(findMaxConsecutiveOnes([1,1,0,1,1,1]))
