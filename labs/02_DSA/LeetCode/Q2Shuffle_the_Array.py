8a371614879b3dfdbb6eb2197fa0dd85a4c7b074
ricardo <101021873+ricardorodrigues-60hz@users.noreply.github.com>
2026-09-14 17:15:43 -0300

    Q2 Shuffle_theArray

--- labs/02_DSA/LeetCode/Q2Shuffle_the_Array.py
+++ labs/02_DSA/LeetCode/Q2Shuffle_the_Array.py
@@ -6,8 +6,9 @@ def shuffle(nums: List[int], n: int) -> List[int]:
     nums = nums[n:]
     print(nums2)
     print(nums)
+    
 
-    return List(lambda l: ...)
+    return [item for pair in zip(nums, nums2) for item in pair]
 
 shuffle([1,2,3,4,5,6], 3)
 
